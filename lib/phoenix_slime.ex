f4c93b3e5845637f07f4ffee7868a2155a902e4c
The Peoples' Bourgeois <850082+thepeoplesbourgeois@users.noreply.github.com>
2022-07-16 16:44:02 -0700

    Enable usage within LiveComponent modules
    Adds a signature to `PhoenixSlime.sigil_L/2` which accepts
    strings tagged with the options charlist `'heex', compiling
    those Slime template markup strings into `heex` templates
    by way of the `Phoenix.LiveView.HTMLEngine`. This, in turn,
    enables Slime to be used as the markup language within the
    `render/1` function of modules that `use Phoenix.Component`
    or `use Phoenix.LiveComponent`.
    
    No corresponding function is made for `sigil_l/2`, as it is
    assumed that any component written for a Phoenix app will
    have some amount of dynamic content, and all dynamic content
    in any Phoenix component must go through the interpolation
    mechanisms provided by the `heex` template schema.

--- lib/phoenix_slime.ex
+++ lib/phoenix_slime.ex
@@ -16,13 +16,28 @@ defmodule PhoenixSlime do
   end
 
   @doc """
-  Provides the `~L` sigil with HTML safe Slime syntax inside source files.
+  Provides the `~L` sigil for compiling Slime markup into `eex` or `heex` template code.
 
       iex> import PhoenixSlime
       iex> ~L"\""
       ...> p hello \#{"world"}
       ...> "\""
       {:safe, ["<p>hello ", "world", "</p>"]}
+
+  To use Slime within LiveComponents, where the result must be validated
+  by `Phoenix.LiveView.HTMLEngine`, the expression may be tagged with
+  the charlist `'heex'`, which will cause the Slime markup to compile
+  to a `Phoenix.LiveView.Rendered` struct.
+
+      iex> import PhoenixSlime
+      iex> assigns = %{world: "world"}
+      iex> rendered = ~L"\""
+      ...> p hello \#{@world}
+      ...> "\""heex
+      %Phoenix.LiveView.Rendered{
+        rendered |
+        static: ["<p>hello ", "</p>"]
+      }
   """
   defmacro sigil_L(expr, opts) do
     handle_sigil(expr, opts, __CALLER__.line)
@@ -34,10 +49,34 @@ defmodule PhoenixSlime do
     |> EEx.compile_string(engine: Phoenix.HTML.Engine, line: line + 1)
   end
 
+  defp handle_sigil({:<<>>, _, [expr]}, 'heex', line) do
+    expr
+     |> Slime.Renderer.precompile_heex()
+     |> EEx.compile_string(engine: Phoenix.LiveView.HTMLEngine, line: line + 1)
+  end
+
   defp handle_sigil(_, _, _) do
     raise ArgumentError,
           ~S(Templating is not allowed with #{} in ~l sigil.) <>
             ~S( Remove the #{}, use = to insert values, or ) <>
             ~S(use ~L to template with #{}.)
   end
+
+  # NOT A GREAT IDEA: conflicts with `Phoenix.LiveView.Helpers`'s own `sigil_H`
+  # KEEPING IT HERE ANYWAY: in case the `heex` charlist tag above feels kludgey in the end
+
+  # @doc ~S"""
+  # Provides .heex compatibility with the `~L` sigil with HTML safe Slime syntax for components.
+
+  # Enables using Slime to build live components
+
+  #     iex> import PhoenixSlime
+  #     iex> assigns = %{world: "world"}
+  #     iex> rendered = ~H[p hello #{@world}]
+  #     iex> rendered.static
+  #     ["<p>hello ", "</p>"]
+  # """
+  # defmacro sigil_H(expr, _opts) do
+  #   handle_sigil(expr, 'heex', __CALLER__.line)
+  # end
 end
